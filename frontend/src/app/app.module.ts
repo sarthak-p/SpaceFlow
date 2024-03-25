11a18bce0607d254dc1083c8f3be1ab492649f7f
HecticJawa <joshburroughs01@gmail.com>
2024-03-25 16:04:42 -0400

    home and teams page prototypes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TeamsComponent } from './teams/teams.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TeamsComponent
   ],
   imports: [
     BrowserModule,
