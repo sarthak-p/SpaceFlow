d5d550a87db082d45945c8ced376298fc9e096d9
Sarthak Patipati <pusarthak@gmail.com>
2024-03-25 15:36:39 -0500

    Added border for login

---
+++ backend/target/generated-sources/annotations/com/cooksys/groupfinal/mappers/CredentialsMapperImpl.java
@@ -0,0 +1,29 @@
+package com.cooksys.groupfinal.mappers;
+
+import com.cooksys.groupfinal.dtos.CredentialsDto;
+import com.cooksys.groupfinal.entities.Credentials;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-03-25T15:26:00-0500",
+    comments = "version: 1.4.1.Final, compiler: javac, environment: Java 17.0.10 (Oracle Corporation)"
+)
+@Component
+public class CredentialsMapperImpl implements CredentialsMapper {
+
+    @Override
+    public Credentials dtoToEntity(CredentialsDto credentialsDto) {
+        if ( credentialsDto == null ) {
+            return null;
+        }
+
+        Credentials credentials = new Credentials();
+
+        credentials.setUsername( credentialsDto.getUsername() );
+        credentials.setPassword( credentialsDto.getPassword() );
+
+        return credentials;
+    }
+}
