4ef9ba2a4df9cdafdc33bf8a8a191b8ce8272f6e
J-Eichhorn <joshuaeichhorn@ymail.com>
2024-03-26 11:43:34 -0400

    creates getTeamById endpoint

--- backend/src/main/java/com/cooksys/groupfinal/controllers/TeamController.java
+++ backend/src/main/java/com/cooksys/groupfinal/controllers/TeamController.java
@@ -1,8 +1,11 @@
 package com.cooksys.groupfinal.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cooksys.groupfinal.dtos.TeamDto;
 import com.cooksys.groupfinal.services.TeamService;
 
 import lombok.RequiredArgsConstructor;
@@ -14,4 +17,8 @@ public class TeamController {
 	
 	private final TeamService teamService;
 
+	@GetMapping("/{id}")
+	public TeamDto getTeamById(@PathVariable Long id) {
+		return teamService.getTeamById(id);
+	}
 }
--- backend/src/main/java/com/cooksys/groupfinal/services/impl/TeamServiceImpl.java
+++ backend/src/main/java/com/cooksys/groupfinal/services/impl/TeamServiceImpl.java
@@ -1,7 +1,14 @@
 package com.cooksys.groupfinal.services.impl;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import com.cooksys.groupfinal.dtos.TeamDto;
+import com.cooksys.groupfinal.entities.Team;
+import com.cooksys.groupfinal.exceptions.NotFoundException;
+import com.cooksys.groupfinal.mappers.TeamMapper;
+import com.cooksys.groupfinal.repositories.TeamRepository;
 import com.cooksys.groupfinal.services.TeamService;
 
 import lombok.RequiredArgsConstructor;
@@ -9,5 +16,20 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class TeamServiceImpl implements TeamService {
+	
+	private final TeamRepository teamRepository;
+	private final TeamMapper teamMapper;
+	
+	
+	@Override
+	public TeamDto getTeamById(Long id) {
+        Optional<Team> optionalTeam = teamRepository.findById(id);
+        if (optionalTeam.isEmpty()) {
+            throw new NotFoundException("A team with the provided id does not exist.");
+        }
+        Team team = optionalTeam.get();
+        		
+        return teamMapper.entityToDto(team);
+	}
 
 }
