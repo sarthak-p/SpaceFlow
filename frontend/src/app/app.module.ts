f8289eca85748488a640a73c7d274284e412c625
Sarthak Patipati <pusarthak@gmail.com>
2024-03-26 15:23:20 -0500

    user-registry updates

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -11,18 +10,10 @@ import { LoginComponent } from './login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { SelectCompanyComponent } from './select-company/select-company.component';
-<<<<<<< Updated upstream
 import { ProjectsComponent } from './projects/projects.component';
 import { CreateProjectComponent } from './create-project/create-project.component';
 import { EditProjectComponent } from './edit-project/edit-project.component';
-=======
 import { UserRegistryComponent } from './user-registry/user-registry.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatCardModule } from '@angular/material/card';
-import { MatTableModule } from '@angular/material/table';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
->>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
@@ -33,24 +24,17 @@ import { MatIconModule } from '@angular/material/icon';
     CreateTeamComponent,
     LoginComponent,
     SelectCompanyComponent,
-<<<<<<< Updated upstream
     ProjectsComponent,
     CreateProjectComponent,
-    EditProjectComponent
-=======
+    EditProjectComponent,
     UserRegistryComponent
->>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    MatToolbarModule,
-    MatCardModule,
-    MatTableModule,
-    MatButtonModule,
-    MatIconModule
+    // Do not include Angular Material modules as requested
   ],
   providers: [],
   bootstrap: [AppComponent]
