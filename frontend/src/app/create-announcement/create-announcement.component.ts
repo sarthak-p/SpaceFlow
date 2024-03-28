564d8ec8b98b27c71e7bf2691ec78aecac6e2c0d
HecticJawa <joshburroughs01@gmail.com>
2024-03-28 15:03:52 -0400

    Added company dropdown selection functionality for Create-Announcement

--- frontend/src/app/create-announcement/create-announcement.component.ts
+++ frontend/src/app/create-announcement/create-announcement.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { AnnouncementService } from '../announcement.service';
+import { CompanyService } from '../company.service';
 import { Announcement } from '../announcement.model';
+import { Company } from '../company.model';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,17 +13,37 @@ import { Router } from '@angular/router';
 export class CreateAnnouncementComponent {
 
   currentUserName: string = "pinky"; // This should be updated by a global app variable on init
-  currentCompanyId: number = 6; // This should be updated by a global app variable on init
+  currentCompanyId: number = 99; // This should be updated by the dropdown menu
   title: string = "title";
   description: string = "description";
-  constructor(private router: Router, private announcementService: AnnouncementService) { }
+
+  companies: Company[] = [];
+  selectedOption!: Company;
+
+  constructor(private companyService: CompanyService, private router: Router, private announcementService: AnnouncementService) { }
 
   goToAnnouncementPage(): void {
     this.router.navigate(['/home']);
   }
 
   ngOnInit() {
-    // Update user and company based on global fields
+    this.fetchCompanies();
+  }
+
+  fetchCompanies() {
+    this.companyService.getCompanies().subscribe(allcompanies => {
+      console.log(allcompanies); 
+      for (const company of allcompanies) {
+        this.companies.push(company);
+      }
+    }, error => console.error(error));
+  }
+
+  onDropdownChange(value: Company) {
+    this.selectedOption = value;
+    this.currentCompanyId = this.selectedOption.id;
+    console.log("MANUAL COMPANY SELECT: ");
+    console.log(this.currentCompanyId);
   }
 
   onSubmit() {
